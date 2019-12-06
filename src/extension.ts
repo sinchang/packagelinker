0801d30a49b774d90ace32a31e115e74db73a6c8
sinchang <sinchangwen@gmail.com>
2019-12-06 15:53:10 +0800

    update api url and support use extension on specific file suffix

--- src/extension.ts
+++ src/extension.ts
@@ -4,53 +4,78 @@ import * as vscode from 'vscode'
 import axios from 'axios'
 const opn = require('opn')
 
-const fileTypes = [{
-  fileName: 'package.json',
-  registry: 'npm'
-}, {
-  fileName: 'requirements.txt',
-  registry: 'pypi'
-}, {
-  fileName: 'composer.json',
-  registry: 'composer'
-}, {
-  fileName: 'Gemfile',
-  registry: 'rubygems'
-}]
+const fileTypes = [
+  {
+    fileName: 'package.json',
+    suffixs: ['ts', 'tsx', 'js', 'html', 'css', 'less', 'sass', 'scss'],
+    registry: 'npm'
+  },
+  {
+    fileName: 'requirements.txt',
+    suffixs: ['py'],
+    registry: 'pypi'
+  },
+  {
+    fileName: 'composer.json',
+    suffixs: ['php'],
+    registry: 'composer'
+  },
+  {
+    fileName: 'Gemfile',
+    suffixs: ['rb'],
+    registry: 'rubygems'
+  }
+]
+
+function getFileExtension(filename) {
+  return filename.split('.').pop()
+}
 
 export function activate(context: vscode.ExtensionContext) {
-  const disposable = vscode.commands.registerCommand('extension.goToGitHub', () => {
-    const editor = vscode.window.activeTextEditor
+  const disposable = vscode.commands.registerCommand(
+    'extension.goToGitHub',
+    () => {
+      const editor = vscode.window.activeTextEditor
 
-    if (!editor) {
-      return
-    }
-
-    const selection = editor.selection
-    const text = editor.document.getText(selection)
-    const currentlyOpenTabfilePath = editor.document.fileName
-    let split = currentlyOpenTabfilePath.split('/')
-		if (split[0] === currentlyOpenTabfilePath) // Windows
-			split = currentlyOpenTabfilePath.split('\\')
-		const currentFileName = split[split.length - 1]
-
-    let registry = ''
-
-    fileTypes.forEach(item => {
-      if (item.fileName === currentFileName) {
-        registry = item.registry
+      if (!editor) {
+        return
       }
-    })
 
-    if (!registry || !text) return
+      const selection = editor.selection
+      const text = editor.document.getText(selection)
+      const currentlyOpenTabfilePath = editor.document.fileName
+      let split = currentlyOpenTabfilePath.split('/')
+      if (split[0] === currentlyOpenTabfilePath)
+        // Windows
+        split = currentlyOpenTabfilePath.split('\\')
+      const currentFileName = split[split.length - 1]
+      const suffix = getFileExtension(currentFileName)
 
-    axios.get(`https://githublinker.herokuapp.com/q/${registry}/${text}`)
-      .then(res => opn(res.data.url))
-      .catch(error => vscode.window.showInformationMessage('Something Error, Please Try Again!'))
-  })
+      let registry = ''
+
+      fileTypes.forEach(item => {
+        if (
+          item.fileName === currentFileName ||
+          item.suffixs.includes(suffix)
+        ) {
+          registry = item.registry
+        }
+      })
+
+      if (!registry || !text) return
+
+      axios
+        .get(`https://octolinker-api.now.sh?${registry}=${text}`)
+        .then(res => {
+          if (res.data.result && res.data.result.length > 0) {
+            opn(res.data.result[0].result)
+          }
+        })
+        .catch(error => vscode.window.showInformationMessage(error.message))
+    }
+  )
 
   context.subscriptions.push(disposable)
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
